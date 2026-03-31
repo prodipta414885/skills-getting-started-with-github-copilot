b9aa045d6ad45541dd00e51beff0b4028ce17cbb
prodipta414885 <prodipta.mukherjee@cgi.com>
2026-03-31 06:47:40 +0000

    Add comment for clarity in activity database section

--- src/app.py
+++ src/app.py
@@ -20,6 +20,7 @@ app.mount("/static", StaticFiles(directory=os.path.join(Path(__file__).parent,
           "static")), name="static")
 
 # In-memory activity database
+##comment by Prodipta
 activities = {
     "Chess Club": {
         "description": "Learn strategies and compete in chess tournaments",
